52b9e5ba40f292d100e05e3bd250f02f6a57120d
Saurabhshe <30689457+Saurabhshe@users.noreply.github.com>
2017-08-21 14:19:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+   title: 'Yash Tutorials' ,
+   heading: 'Yash Tutorials Learn like never before Best learning experience for all standards. Personalised coaching. Healthy atmosphere. Its s time to learn. find your own learning path ',
+   content: `<p>
+                We are committed to take Education to every home through our teachers by using technology in the following areas of the education eco-system.
+            </p>
+            <ol>
+                <li>Digital content for Learning, Teaching and Assessment</li>
+                <li>Innovative Learning and teaching methodologies such as blended learning and flipped classrooms
+                <li>Flexible delivery models of education on different technology & end-user platforms</li>
+            </ol>' `
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+var htmltemplate = `<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <title>
+                ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <div class="header">
+            <div id="header-text">
+                ${heading}
+            </div>
+        </div>
+        <div class="content">
+                ${content}
+        </div>
+    </body>
+</html>
+`;
+ return htmltemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(createtemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
